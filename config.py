eed4ba4083194fdc3b65d2f28aa17c33d536d7b2
khosrotajik480-droid <khosrotajik480@gmail.com>
2026-07-21 20:53:49 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+# ==========================
+# تنظیمات ربات
+# ==========================
+
+# توکن ربات (بعداً در Render وارد می‌شود)
+TOKEN = os.getenv("BOT_TOKEN")
+
+# آیدی عددی ادمین
+ADMIN_ID = 6284338325
+
+# کانال اصلی (برای عضویت اجباری)
+CHANNEL_USERNAME = "@Anime_G_M"
+
+# کانال آرشیو
+# فعلاً بعداً Chat ID آن را قرار می‌دهیم
+STORAGE_CHANNEL = os.getenv("STORAGE_CHANNEL")
