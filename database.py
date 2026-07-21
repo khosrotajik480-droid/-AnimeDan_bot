0395961b9dffe798b89375cdb61df8029b4e0b5e
khosrotajik480-droid <khosrotajik480@gmail.com>
2026-07-21 21:20:07 +0330

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,20 @@
 import sqlite3
 
-DATABASE_NAME = "database.db"
+DATABASE_NAME = "anime_bot.db"
+
+
 def connect():
 
     conn = sqlite3.connect(DATABASE_NAME)
 
+    conn.row_factory = sqlite3.Row
+
     cursor = conn.cursor()
 
     return conn, cursor
-  def create_users():
+
+
+def create_database():
 
     conn, cursor = connect()
 
@@ -24,18 +30,11 @@ def connect():
 
         username TEXT,
 
-        join_date TEXT
+        joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
     )
 
     """)
-
-    conn.commit()
-
-    conn.close()
-    def create_animes():
-
-    conn, cursor = connect()
 
     cursor.execute("""
 
@@ -49,13 +48,6 @@ def connect():
 
     """)
 
-    conn.commit()
-
-    conn.close()
-    def create_seasons():
-
-    conn, cursor = connect()
-
     cursor.execute("""
 
     CREATE TABLE IF NOT EXISTS seasons(
@@ -64,18 +56,15 @@ def connect():
 
         anime_id INTEGER,
 
-        season_number INTEGER
+        season_number INTEGER,
+
+        FOREIGN KEY(anime_id)
+
+        REFERENCES animes(id)
 
     )
 
     """)
-
-    conn.commit()
-
-    conn.close()
-    def create_episodes():
-
-    conn, cursor = connect()
 
     cursor.execute("""
 
@@ -87,9 +76,13 @@ def connect():
 
         episode_number INTEGER,
 
-        chat_id INTEGER,
+        file_id TEXT,
 
-        message_id INTEGER
+        caption TEXT,
+
+        FOREIGN KEY(season_id)
+
+        REFERENCES seasons(id)
 
     )
 
@@ -98,64 +91,126 @@ def connect():
     conn.commit()
 
     conn.close()
-    def create_database():
+    def add_user(
 
-    create_users()
+    telegram_id,
 
-    create_animes()
+    first_name,
 
-    create_seasons()
+    username
 
-    create_episodes()
-    from datetime import datetime
-
-
-def add_user(telegram_id, first_name, username):
+):
 
     conn, cursor = connect()
 
-    cursor.execute("""
+    cursor.execute(
 
-    INSERT OR IGNORE INTO users(
+        """
 
-        telegram_id,
+        INSERT OR IGNORE INTO users(
 
-        first_name,
+            telegram_id,
 
-        username,
+            first_name,
 
-        join_date
+            username
 
-    )
+        )
 
-    VALUES(?,?,?,?)
+        VALUES(?,?,?)
 
-    """,
+        """,
 
-    (
+        (
 
-        telegram_id,
+            telegram_id,
 
-        first_name,
+            first_name,
 
-        username,
+            username
 
-        datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    )
+        )
 
     )
 
     conn.commit()
 
     conn.close()
-  def users_count():
+
+
+def get_user(
+
+    telegram_id
+
+):
 
     conn, cursor = connect()
 
     cursor.execute(
 
-        "SELECT COUNT(*) FROM users"
+        """
+
+        SELECT *
+
+        FROM users
+
+        WHERE telegram_id=?
+
+        """,
+
+        (
+
+            telegram_id,
+
+        )
+
+    )
+
+    data = cursor.fetchone()
+
+    conn.close()
+
+    return data
+
+
+def get_users():
+
+    conn, cursor = connect()
+
+    cursor.execute(
+
+        """
+
+        SELECT *
+
+        FROM users
+
+        ORDER BY id DESC
+
+        """
+
+    )
+
+    data = cursor.fetchall()
+
+    conn.close()
+
+    return data
+
+
+def users_count():
+
+    conn, cursor = connect()
+
+    cursor.execute(
+
+        """
+
+        SELECT COUNT(*)
+
+        FROM users
+
+        """
 
     )
 
@@ -164,379 +219,3 @@ def add_user(telegram_id, first_name, username):
     conn.close()
 
     return count
-    def add_anime(name):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        "INSERT INTO animes(name) VALUES(?)",
-
-        (name,)
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def get_animes():
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        "SELECT * FROM animes"
-
-    )
-
-    data = cursor.fetchall()
-
-    conn.close()
-
-    return data
-    def get_anime_id(name):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        "SELECT id FROM animes WHERE name=?",
-
-        (name,)
-
-    )
-
-    data = cursor.fetchone()
-
-    conn.close()
-
-    if data:
-
-        return data[0]
-
-    return None
-    def add_season(anime_id, season):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        INSERT INTO seasons(
-
-        anime_id,
-
-        season_number
-
-        )
-
-        VALUES(?,?)
-
-        """,
-
-        (
-
-            anime_id,
-
-            season
-
-        )
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def get_seasons(anime_id):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        SELECT season_number
-
-        FROM seasons
-
-        WHERE anime_id=?
-
-        ORDER BY season_number
-
-        """,
-
-        (anime_id,)
-
-    )
-
-    data = cursor.fetchall()
-
-    conn.close()
-
-    return data
-    def add_episode(
-    season_id,
-    episode_number,
-    chat_id,
-    message_id
-):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        INSERT INTO episodes(
-
-            season_id,
-
-            episode_number,
-
-            chat_id,
-
-            message_id
-
-        )
-
-        VALUES(?,?,?,?)
-
-        """,
-
-        (
-
-            season_id,
-
-            episode_number,
-
-            chat_id,
-
-            message_id
-
-        )
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def get_episodes(season_id):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        SELECT
-
-        episode_number,
-
-        chat_id,
-
-        message_id
-
-        FROM episodes
-
-        WHERE season_id=?
-
-        ORDER BY episode_number
-
-        """,
-
-        (season_id,)
-
-    )
-
-    data = cursor.fetchall()
-
-    conn.close()
-
-    return data
-    def get_episode(season_id, episode_number):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        SELECT
-
-        chat_id,
-
-        message_id
-
-        FROM episodes
-
-        WHERE season_id=?
-
-        AND episode_number=?
-
-        """,
-
-        (
-
-            season_id,
-
-            episode_number
-
-        )
-
-    )
-
-    data = cursor.fetchone()
-
-    conn.close()
-
-    return data
-    def delete_episode(season_id, episode_number):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        DELETE FROM episodes
-
-        WHERE season_id=?
-
-        AND episode_number=?
-
-        """,
-
-        (
-
-            season_id,
-
-            episode_number
-
-        )
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def delete_season(season_id):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        "DELETE FROM episodes WHERE season_id=?",
-
-        (season_id,)
-
-    )
-
-    cursor.execute(
-
-        "DELETE FROM seasons WHERE id=?",
-
-        (season_id,)
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def delete_anime(anime_id):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        "SELECT id FROM seasons WHERE anime_id=?",
-
-        (anime_id,)
-
-    )
-
-    seasons = cursor.fetchall()
-
-    for season in seasons:
-
-        cursor.execute(
-
-            "DELETE FROM episodes WHERE season_id=?",
-
-            (season[0],)
-
-        )
-
-    cursor.execute(
-
-        "DELETE FROM seasons WHERE anime_id=?",
-
-        (anime_id,)
-
-    )
-
-    cursor.execute(
-
-        "DELETE FROM animes WHERE id=?",
-
-        (anime_id,)
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    def update_episode(
-
-    season_id,
-
-    episode_number,
-
-    chat_id,
-
-    message_id
-
-):
-
-    conn, cursor = connect()
-
-    cursor.execute(
-
-        """
-
-        UPDATE episodes
-
-        SET
-
-        chat_id=?,
-
-        message_id=?
-
-        WHERE
-
-        season_id=?
-
-        AND
-
-        episode_number=?
-
-        """,
-
-        (
-
-            chat_id,
-
-            message_id,
-
-            season_id,
-
-            episode_number
-
-        )
-
-    )
-
-    conn.commit()
-
-    conn.close()
-    
